262c20af9abd108ad78810e6f9901445ec869ee9
Mink-a <rahulakhant@gmail.com>
2022-04-12 20:48:56 +0630

    Generate random choices for computerSection

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 console.log('Hello from console!')
+
+function computerPlay(){
+    choices = ['rock', 'paper', 'scissors']
+
+return choices[Math.floor(Math.random() * 3)]
+}
+
+console.log(computerPlay())
