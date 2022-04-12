b2d31478a549d27fc66c859f4a5b9a596f01eca1
Mink-a <rahulakhant@gmail.com>
2022-04-12 21:08:40 +0630

    Test playRound function with one condition

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ console.log('Hello from console!')
 function computerPlay(){
     choices = ['rock', 'paper', 'scissors']
 
-return choices[Math.floor(Math.random() * 3)]
+return String(choices[Math.floor(Math.random() * 3)])
 }
 
-console.log(computerPlay())
+// console.log(computerPlay())
+
+function playRound(playerSection, computerSection){
+    if (playerSection === computerSection){
+        return `Draw! ${playerSection} and ${computerSection} are the same`
+    }else{
+        return `Win! ${playerSection} beats ${computerSection}`
+    }
+}
+
+console.log(playRound('rock', computerPlay()))
