c0046a0eeb96325df56bf1c58b31735043ead45d
Mink-a <rahulakhant@gmail.com>
2022-04-12 21:50:46 +0630

    Create playRound function with full conditons

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-console.log('Hello from console!')
+console.log("Hello from console!");
 
-function computerPlay(){
-    choices = ['rock', 'paper', 'scissors']
+function computerPlay() {
+  choices = ["rock", "paper", "scissors"];
 
-return String(choices[Math.floor(Math.random() * 3)])
+  return String(choices[Math.floor(Math.random() * 3)]);
 }
 
 // console.log(computerPlay())
 
-function playRound(playerSection, computerSection){
-    if (playerSection === computerSection){
-        return `Draw! ${playerSection} and ${computerSection} are the same`
-    }else{
-        return `Win! ${playerSection} beats ${computerSection}`
-    }
+function playRound(playerSection, computerSection) {
+  if (playerSection === computerSection) {
+    return `Draw! ${playerSection} and ${computerSection} are the same`;
+  } else if (
+    (playerSection === "rock" && computerSection === "scissors") ||
+    (playerSection === "paper" && computerSection === "rock") ||
+    (playerSection === "scissors" && computerSection === "paper")
+  ) {
+    return `You Win! ${playerSection} beats ${computerSection}`;
+  } else {
+    return `You Lose! ${computerSection} beats ${playerSection}`;
+  }
 }
 
-console.log(playRound('rock', computerPlay()))
+console.log(
+  playRound(prompt("Enter your move: ").toLowerCase(), computerPlay())
+);
