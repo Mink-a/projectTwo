f7dab809b8cdf24f6fcc8748eaa1735e1ec6bfda
Mink-a <rahulakhant@gmail.com>
2022-04-13 08:15:02 +0630

    Create game() function that return the whole game winner

--- js/main.js
+++ js/main.js
@@ -8,20 +8,52 @@ function computerPlay() {
 
 // console.log(computerPlay())
 
+var gamesYouWin = 0;
+var gamesYouLose = 0;
+
 function playRound(playerSection, computerSection) {
   if (playerSection === computerSection) {
+    console.log(
+      "You Win " + gamesYouWin + " : " + "Computer Win " + gamesYouLose
+    );
     return `Draw! ${playerSection} and ${computerSection} are the same`;
   } else if (
     (playerSection === "rock" && computerSection === "scissors") ||
     (playerSection === "paper" && computerSection === "rock") ||
     (playerSection === "scissors" && computerSection === "paper")
   ) {
+    gamesYouWin++;
+    console.log(
+      "You Win " + gamesYouWin + " : " + "Computer Win " + gamesYouLose
+    );
     return `You Win! ${playerSection} beats ${computerSection}`;
   } else {
+    gamesYouLose++;
+    console.log(
+      "You Win " + gamesYouWin + " : " + "Computer Win " + gamesYouLose
+    );
     return `You Lose! ${computerSection} beats ${playerSection}`;
   }
 }
 
-console.log(
-  playRound(prompt("Enter your move: ").toLowerCase(), computerPlay())
-);
+// console.log(
+//   playRound(prompt("Enter your move: ").toLowerCase(), computerPlay())
+// );
+
+function game() {
+  for (let games = 0; games < 5; games++) {
+    console.log(
+      playRound(prompt("Enter your move: ").toLowerCase(), computerPlay())
+    );
+  }
+
+  if (gamesYouWin === gamesYouLose) {
+    console.log("Draw!!!");
+  } else if (gamesYouWin > gamesYouLose) {
+    console.log("You Win the whole game!");
+  } else {
+    console.log("You Lose Kid!");
+  }
+}
+
+console.log(game());
